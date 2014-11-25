49638dd187ffabea76e05d44d43409f31085c0e7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-11-25 23:24:01 +0100

    Create master

--- TodoFrontend/web/js/todo.js
+++
@@ -1,9 +0,0 @@
-var Student = Student || {};
-(function(exports) {
-    function displayUI() {
-        // TODO : votre code ici ;-)
-        var el = '<li class="list-group-item" style="background-color:white;" ><div class="row"><div class="col-md-9"><h3><span>Votre application ici ;-)</span></h3></div><div class="col-md-3"></div></div></li>';
-        $('#todo').html(el);
-    }
-    exports.displayUI = displayUI;
-})(Student);
